c716bd31932e88ecde14a411cd05e3d336be3b0f
abhilash <21965736+vorzawk@users.noreply.github.com>
2018-07-03 13:36:48 -0500

    read the input data from mnist

---
+++ readData.py
@@ -0,0 +1,6 @@
+import numpy as np
+from matplotlib import pyplot as plt
+data = np.genfromtxt('train.csv',delimiter=',',skip_header=1, max_rows=2)
+img = np.reshape(data[0][1:], newshape=(28,28))
+plt.imshow(img, interpolation='nearest', cmap='gray')
+plt.show()
