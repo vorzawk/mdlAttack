4a08cab9237e4424383c6265b3fc6e6af356cbe6
abhilash <21965736+vorzawk@users.noreply.github.com>
2018-08-06 12:38:54 -0500

    tf module added

--- readData.py
+++
@@ -1,7 +0,0 @@
-""" read the data in csv format and display as an image """
-import numpy as np
-from matplotlib import pyplot as plt
-data = np.genfromtxt('train.csv',delimiter=',',skip_header=1, max_rows=2)
-img = np.reshape(data[0][1:], newshape=(28,28))
-plt.imshow(img, interpolation='nearest', cmap='gray')
-plt.show()
